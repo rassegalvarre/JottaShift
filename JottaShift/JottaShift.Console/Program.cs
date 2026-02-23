15e14ed0550ffca80860da3bfa0df1c7c784f75c
kristoffer-heia <kristoffer.heia@skjold>
2026-02-23 15:27:07 +0100

    List variables

--- JottaShift/JottaShift.Console/Program.cs
+++ JottaShift/JottaShift.Console/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System.IO.Abstractions;
+using JottaShift.Core.Configuration;
 
 Console.WriteLine("JottaShift initiating..");
 
@@ -37,30 +38,37 @@ var host = Host.CreateDefaultBuilder(args)
 
 await host.StartAsync();
 
-using var scope = host.Services.CreateScope();
-var exporter = scope.ServiceProvider.GetRequiredService<ITimelineExport>();
+EnvironmentVariableManager.InitializeEnvironmentVariables();
 
-var config = new ConfigurationBuilder()
-    .SetBasePath(Directory.GetCurrentDirectory()) 
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .Build();
+bool doExport = false;
 
-var options = new TimelineExportOptions(
-    SourceRoot: config.GetValue<string>("SourceRoot")!,
-    DestinationRoot: config.GetValue<string>("DestinationRoot")!);
+if (doExport)
+{
+    using var scope = host.Services.CreateScope();
+    var exporter = scope.ServiceProvider.GetRequiredService<ITimelineExport>();
 
-Console.WriteLine($"Source directory to copy from:      {options.SourceRoot}");
-Console.WriteLine($"Destination directory to copy to:   {options.DestinationRoot}");
+    var config = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory()) 
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .Build();
 
-Console.WriteLine("Starting timeline export...");
+    var options = new TimelineExportOptions(
+        SourceRoot: config.GetValue<string>("SourceRoot")!,
+        DestinationRoot: config.GetValue<string>("DestinationRoot")!);
 
-// Export and re-strucuture the timeline-staging to storage folder
-await exporter.ExportAsync(options, new CancellationToken());
-// TODO: Delete contents in staging after export
+    Console.WriteLine($"Source directory to copy from:      {options.SourceRoot}");
+    Console.WriteLine($"Destination directory to copy to:   {options.DestinationRoot}");
 
-Console.WriteLine("Timeline export finished");
+    Console.WriteLine("Starting timeline export...");
 
-// TODO: Read from Chromecast-staging and upload to Google Photos
-// Delete contents in staging after successfull upload
+    // Export and re-strucuture the timeline-staging to storage folder
+    await exporter.ExportAsync(options, new CancellationToken());
+    // TODO: Delete contents in staging after export
+
+    Console.WriteLine("Timeline export finished");
+
+    // TODO: Read from Chromecast-staging and upload to Google Photos
+    // Delete contents in staging after successfull upload
+}
 
 await host.StopAsync();
---
+++ JottaShift/JottaShift.Core/Configuration/EnvironmentVariableManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JottaShift.Core.Configuration;
+
+public class EnvironmentVariableManager
+{
+    public static string? GooglePhotosLibraryApiProjectId => Environment.GetEnvironmentVariable("GooglePhotosLibraryApi_ProjectId");
+    public static string? GooglePhotosLibraryApiClientId => Environment.GetEnvironmentVariable("GooglePhotosLibraryApi_ClientId");
+    public static string? GooglePhotosLibraryApiClientSecret => Environment.GetEnvironmentVariable("GooglePhotosLibraryApi_ClientSecret");
+    public static string? SteamWebApiClientApiKey = Environment.GetEnvironmentVariable("SteamWebApi_ClientApiKey");
+    public static string? SteamWebApiStoreLanguage = Environment.GetEnvironmentVariable("SteamWebApi_StoreLanguage");
+
+    private static readonly string?[] _environmentVariableNames =
+    [
+        GooglePhotosLibraryApiProjectId,
+        GooglePhotosLibraryApiClientId,
+        GooglePhotosLibraryApiClientSecret,
+        SteamWebApiClientApiKey,
+        SteamWebApiStoreLanguage,
+    ];
+
+    // Prompt each variable from the user, and set it in the environment 
+    public static void InitializeEnvironmentVariables()
+    {
+        foreach (var environmentVariable in _environmentVariableNames)
+        {
+            Console.WriteLine(environmentVariable);
+            //if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable(environmentVariable)))
+            //{
+            //    continue; // Skip if already set
+            //}
+
+            //Console.Write($"Enter value for {environmentVariable}: ");
+            //var value = Console.ReadLine();
+            //if (string.IsNullOrEmpty(value))
+            //{
+            //    Console.WriteLine($"Value for {environmentVariable} cannot be empty. Please try again.");
+            //    return;
+            //}
+            //Environment.SetEnvironmentVariable(environmentVariable, value);
+        }
+    }
+}
