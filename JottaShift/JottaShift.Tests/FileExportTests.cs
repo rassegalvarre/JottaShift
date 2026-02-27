2b1422cbd510fe6dd0e11ca419519b0599359c47
Kristoffer Heia <kristoffer.heia@protonmail.com>
2026-02-27 21:54:40 +0100

    Initial test for ExportSteamScreenshotsAsync

--- JottaShift/JottaShift.Tests/FileExportTests.cs
+++ JottaShift/JottaShift.Tests/FileExportTests.cs
@@ -39,7 +39,7 @@ public class FileExportTests
             TargetDirectoryPath = @"C:\backup"
         };
         var settings = new FileExportSettings()
-        { 
+        {
             FileTransferJobs = new List<FileTransferJob>() {
                 job
             }
@@ -57,7 +57,7 @@ public class FileExportTests
             "05 Mai",
             Path.GetFileName(TestData.Duck));
         var duckContent = await File.ReadAllBytesAsync(TestData.Duck);
-        
+
         var waterfallSource = Path.Combine(
             job.SourceDirectoryPath,
             "lorem ipsum",
@@ -94,5 +94,68 @@ public class FileExportTests
         Assert.True(result.Success);
         Assert.True(fileSystemMock.File.Exists(duckTarget));
         Assert.True(fileSystemMock.File.Exists(waterfallTarget));
+    }
+
+    [Fact]
+    public async Task ExportSteamScreenshotsAsync_ShouldExportSteamScreenshots_ToDirectoryWithAppName()
+    {
+
+        var appIdAndNamePair = new Dictionary<uint, string>
+        {
+            { 1, "Pung" },
+            { 12345, "Duum" },
+            { 987653, "Super Mawio" }        
+        };
+
+        var jobSettings = new FileTransferJob()
+        {
+            Key = "steam_screenshots",
+            SourceDirectoryPath = @"C:\steam",
+            TargetDirectoryPath = @"C:\backup\steam"
+        };
+
+        var mockFileData = new Dictionary<string, MockFileData>();
+        var steamRepositoryMock = new Mock<ISteamRepository>();
+
+        foreach (var app in appIdAndNamePair)
+        {
+            mockFileData.Add(
+                Path.Combine(@"C:\steam", app.Key.ToString(), "image.png"),
+                new MockFileData([]));
+        
+            steamRepositoryMock
+                .Setup(repo => repo.GetAppNameFromId(app.Key))
+                .ReturnsAsync(app.Value);        
+        }
+
+        var fileSystemMock = new MockFileSystem(mockFileData);
+
+        var fileStorageService = new FileStorageService(
+            fileSystemMock,
+            new Mock<ILogger<FileStorageService>>().Object);
+
+
+        var fileExportOrchestrator = new FileExportOrchestrator(
+            new FileExportSettings()
+            {
+                FileTransferJobs = new List<FileTransferJob>()
+                {
+                    jobSettings
+                }
+            },
+            new Mock<ILogger<FileExportOrchestrator>>().Object,
+            fileStorageService,
+            new Mock<IGooglePhotosRepository>().Object,
+            steamRepositoryMock.Object);
+
+        var result = await fileExportOrchestrator.ExportSteamScreenshotsAsync();
+
+        Assert.True(result.Success);
+
+        foreach (var app in appIdAndNamePair)
+        {
+            var expectedTargetPath = Path.Combine(jobSettings.TargetDirectoryPath, app.Value, "image.png");
+            Assert.True(fileSystemMock.File.Exists(expectedTargetPath), $"Expected file at path {expectedTargetPath} was not found.");
+        }
     }
 }
