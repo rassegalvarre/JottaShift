fbc622138e36769f7b119a78cbcb283d55f6e147
Kristoffer Heia <kristoffer.heia@protonmail.com>
2026-03-01 19:41:33 +0100

    Added traits

--- JottaShift/JottaShift.Tests/GooglePhotosTests.cs
+++ JottaShift/JottaShift.Tests/GooglePhotosTests.cs
@@ -5,13 +5,13 @@ namespace JottaShift.Tests;
 
 // TODO: We dont want to actually make requests to Google in most of these tests.
 // Test only the connection. How to solve? Create an addition service that mocks the Google Photos API? Or use a library like Moq to mock the service?
+[Trait("API", "Google")]
 public class GooglePhotosTests
 {
     private const string TestAlbumName = "JottaShift.UnitTests";
 
 
     [Fact]
-    [Trait("Category", "CI-Skip")]
     public async Task GetOrCreateAlbum_CreateOrGetsAlbum_WithAlbumName()
     {
         var googlePhotosRepository = new GooglePhotosRepository(new FileSystem());
@@ -21,13 +21,12 @@ public class GooglePhotosTests
     }
 
     [Fact]
-    [Trait("Category", "CI-Skip")]
     public async Task UploadImage_UploadsTestImage()
     {
         var images = new List<string> {
             TestData.Duck,
             TestData.Waterfall
-        };[Trait("Category", "CI-Skip")]
+        };
 
         var googlePhotosRepository = new GooglePhotosRepository(new FileSystem());
 
