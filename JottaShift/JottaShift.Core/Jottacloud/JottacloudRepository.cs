160c2e71931c93c94b574a8ccff3df39bfca4cbc
Kristoffer Heia <kristoffer.heia@protonmail.com>
2026-03-07 21:31:40 +0100

    Initial test

--- JottaShift/JottaShift.Core/Jottacloud/JottacloudRepository.cs
+++ JottaShift/JottaShift.Core/Jottacloud/JottacloudRepository.cs
@@ -5,7 +5,7 @@ namespace JottaShift.Core.Jottacloud;
 
 public class JottacloudRepository(
     ILogger<JottacloudRepository> _logger,
-    IFileStorage fileStorage) : IJottacloudRepository
+    IFileStorage _fileStorage) : IJottacloudRepository
 {
     public async Task<IEnumerable<string>> GetImagesInAlbumAsync(string albumId)
     {
@@ -15,6 +15,14 @@ public class JottacloudRepository(
 
     public async Task<string> GetImageFilePathFromFileName(string imageFileName)
     {
+        var imageDate = _fileStorage.GetImageDate(imageFileName);
+        if (imageDate == default)
+        {
+            return string.Empty;
+        }
+
+
+
         await Task.CompletedTask;
         return string.Empty;
     }
--- JottaShift/JottaShift.Tests/Jottacloud/JottacloudRepositoryTests.cs
+++ JottaShift/JottaShift.Tests/Jottacloud/JottacloudRepositoryTests.cs
@@ -1,4 +1,7 @@
-﻿namespace JottaShift.Tests.Jottacloud;
+﻿using JottaShift.Core.FileStorage;
+using Moq;
+
+namespace JottaShift.Tests.Jottacloud;
 
 public class JottacloudRepositoryTests(JottacloudFixture _fixture)
     : IClassFixture<JottacloudFixture>
@@ -9,16 +12,29 @@ public class JottacloudRepositoryTests(JottacloudFixture _fixture)
     {
         new[] { "img_20100612_163249", Path.Combine(BaseDirectory, "Images", "2010", "6") },
         new[] { "p_20100612_163249", Path.Combine(BaseDirectory, "Images", "2010", "06") },
-        new[] { "v_20100612_163249", Path.Combine(BaseDirectory, "Images", "2012", "June") },
-        new[] { "20100612_163249", Path.Combine(BaseDirectory, "Images", "2012", "06 - June") }
+        new[] { "v_20100612_163249", Path.Combine(BaseDirectory, "Images", "2010", "June") },
+        new[] { "20100612_163249", Path.Combine(BaseDirectory, "Images", "2010", "06 - June") }
     };
-
 
     [Theory]
     [MemberData(nameof(ImageNameAndFullFilePath))]
     public async Task GetImageFilePathFromFileName_ReturnFilePath_WhenDirectoryMatchesFileName(string imageName, string directoryFullPath)
     {
-        var repository = _fixture.CreateJottacloudRepository();
+        var imageDate = new DateTime(2010, 6, 12, 16, 32, 49, DateTimeKind.Local);
+        var imageFullPath = Path.Combine(directoryFullPath, imageName);
+
+        var fileStorageMock = new Mock<IFileStorage>();
+        
+        fileStorageMock.Setup(fs => fs.GetImageDate(It.IsAny<string>()))
+            .Returns(imageDate);
+
+        fileStorageMock.Setup(fs => fs.SearchFileByExactName(
+            It.IsAny<string>(),
+            It.IsAny<string>()))
+            .Returns(imageFullPath);
+
+        var repository = _fixture.CreateJottacloudRepository(
+            fileStorage: fileStorageMock.Object);
 
         string filePath = await repository.GetImageFilePathFromFileName(imageName);
         string expectedFilePath = Path.Combine(directoryFullPath, imageName);
