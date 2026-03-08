905c34cb5cc1e49985861555d6254ee64d680a2c
Kristoffer Heia <kristoffer.heia@protonmail.com>
2026-03-08 22:04:55 +0100

    Remove out-commented

--- JottaShift/JottaShift.Core/Jottacloud/JottacloudRepository.cs
+++ JottaShift/JottaShift.Core/Jottacloud/JottacloudRepository.cs
@@ -45,40 +45,10 @@ public class JottacloudRepository(
         {
             _logger.LogError("Could not deserialize response to the expected schema");
             throw new NotSupportedException("Unexpected response content");
-        }
-
-        //List<string> fileNames = [];
-
-        //foreach (var item in album.Photos)
-        //{
-        //    var imageDate = item.CapturedDate; // Use this to determine image location on disk.
-        //    fileNames.Add(item.Filename);
-        //}
+        }     
 
         return album;
     }
-    
-    //public async Task<IEnumerable<string>> GetLocalPathForImagesInAlbumAsync(string albumId)
-    //{
-    //    List<string> localFilePaths = [];
-
-    //    var images = await GetAlbum(albumId);
-
-    //    foreach (var image in images)
-    //    {
-    //        string localPath = await GetImageFilePathFromFileName(image);
-    //        if (string.IsNullOrEmpty(localPath))
-    //        {
-    //            _logger.LogWarning("Local path for image {ImageName} from album {AlbumId} could not be determined",
-    //                image, albumId);
-    //            continue;
-    //        }
-
-    //        localFilePaths.Add(localPath);
-    //    }
-
-    //    return localFilePaths;
-    //}
 
     public async Task<string> GetImageFilePathFromFileName(string imageFileName)
     {
