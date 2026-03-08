b9cb9b2b18516acfa96bc627f66d822aeb5d18e1
Kristoffer Heia <kristoffer.heia@protonmail.com>
2026-03-08 22:25:12 +0100

    GetAlbumId

--- JottaShift/JottaShift.Core/Jottacloud/JottacloudAdapter.cs
+++ JottaShift/JottaShift.Core/Jottacloud/JottacloudAdapter.cs
@@ -2,6 +2,11 @@
 
 public static class JottacloudAdapter
 {
+    public static string AlbumIdFromSharedUri(Uri sharedUri)
+    {
+        return sharedUri.Segments.Last();
+    }
+
     public static DateTimeOffset PhotoCapturedDateToLocalDateTime(Photo photo)
     {
         return DateTimeOffset.FromUnixTimeMilliseconds(photo.CapturedDate);
--- JottaShift/JottaShift.Tests/Jottacloud/JottacloudAdapterTests.cs
+++ JottaShift/JottaShift.Tests/Jottacloud/JottacloudAdapterTests.cs
@@ -1,12 +1,20 @@
 ﻿using JottaShift.Core.Jottacloud;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace JottaShift.Tests.Jottacloud;
 
 public class JottacloudAdapterTests
 {
+    [Fact]
+    public void AlbumIdFromSharedUri_ReturnsAlbumId()
+    {
+        const string albumId = "mse9823jff12fver";
+        var sharedUri = new Uri($"https://www.jottacloud.com/share/{albumId}");
+
+        var result = JottacloudAdapter.AlbumIdFromSharedUri(sharedUri);
+
+        Assert.Equal(albumId, result);
+    }
+
     [Fact]
     public void PhotoCapturedDateToLocalDateTime_ParsesLongToDateTime()
     {
