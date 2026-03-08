dfa2f25ca75e34c55efde8fdee4ed9454c0adb00
Kristoffer Heia <kristoffer.heia@protonmail.com>
2026-03-08 21:18:16 +0100

    Use adapter

--- JottaShift/JottaShift.Core/Jottacloud/PhotoDto.cs
+++ JottaShift/JottaShift.Core/Jottacloud/PhotoDto.cs
@@ -3,13 +3,13 @@
 public record PhotoDto
 {
     public string ImageName { get; init; }
-    public DateTime CapturedDate { get; init; }
+    public DateTimeOffset CapturedDate { get; init; }
     public string? LocalFilePath { get; init; }
 
     public PhotoDto(Photo photo, string? locationFilePath = null)
     {
         ImageName = photo.Filename;
-        CapturedDate = DateTimeOffset.FromUnixTimeMilliseconds(photo.CapturedDate).LocalDateTime;
+        CapturedDate = JottacloudAdapter.PhotoCapturedDateToLocalDateTime(photo);
         LocalFilePath = locationFilePath;
     }
 }
