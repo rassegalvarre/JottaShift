0a1273abe194dde803192a4d36fb1eda588de54f
Kristoffer Heia <kristoffer.heia@visma.com>
2026-03-10 08:59:58 +0100

    Doc

--- JottaShift/JottaShift.Core/Jottacloud/IJottacloudClient.cs
+++ JottaShift/JottaShift.Core/Jottacloud/IJottacloudClient.cs
@@ -1,6 +1,12 @@
 ﻿namespace JottaShift.Core.Jottacloud;
 
+/// <summary>
+/// Client for accessing Jottacloud API.
+/// </summary>
 public interface IJottacloudClient
 {
+    /// <summary>
+    /// Returns the photo-album with the specified ID.
+    /// </summary>
     Task<Result<Album>> GetAlbumAsync(string albumId, int limit = 100);
 }
--- JottaShift/JottaShift.Core/Jottacloud/IJottacloudRepository.cs
+++ JottaShift/JottaShift.Core/Jottacloud/IJottacloudRepository.cs
@@ -1,6 +1,12 @@
 ﻿namespace JottaShift.Core.Jottacloud;
 
+/// <summary>
+/// Defines methods for accessing and managing files and photo-albums in Jottacloud.
+/// </summary>
 public interface IJottacloudRepository
 {
+    /// <summary>
+    /// Returns a list of photos in the specified album and their local file paths if they exist in local storage.
+    /// </summary>
     Task<IEnumerable<PhotoDto>> GetAlbumPhotos(string albumId);
 }
