e9096ae1a5e99647a97bedfdda0da95fa4641569
Kristoffer Heia <kristoffer.heia@visma.com>
2026-03-11 14:52:34 +0100

    Reorder doc

--- JottaShift/JottaShift.Core/GooglePhotos/IGooglePhotosRepository.cs
+++ JottaShift/JottaShift.Core/GooglePhotos/IGooglePhotosRepository.cs
@@ -5,8 +5,8 @@ public interface IGooglePhotosRepository
     /// <summary>
     /// Uploads a list of photos to Google Storage and adds them to an album.
     /// </summary>
-    /// <param name="photosFullPath">Full file-path for the photos to be uploaded</param>
     /// <param name="albumName">Album full name</param>
+    /// <param name="photosFullPath">Full file-path for the photos to be uploaded</param>
     /// <returns>A <see cref="Result[T]" with the number of images uploaded/></returns>
     Task<Result<int>> UploadPhotosToAlbum(string albumName, IEnumerable<string> photosFullPath);
 }
