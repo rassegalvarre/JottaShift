bf2a9e1fd08aedb1e394cb202089c5073c82c214
Kristoffer Heia <kristoffer.heia@protonmail.com>
2026-03-12 21:38:02 +0100

    Tests for validating environment variables

---
+++ JottaShift/JottaShift.Tests/Configuration/EnvironmentVariableManagerTests.cs
@@ -0,0 +1,26 @@
+﻿using JottaShift.Core.Configuration;
+
+namespace JottaShift.Tests.Configuration;
+
+public class EnvironmentVariableManagerTests
+{
+    [Fact]
+    [Trait("Dependency", "Env")]
+    public void ValidateAllEnvironmentVariablesForApplication()
+    {
+        Assert.False(string.IsNullOrEmpty(
+            EnvironmentVariableManager.GooglePhotosLibraryApiClientId));
+
+        Assert.False(string.IsNullOrEmpty(
+            EnvironmentVariableManager.GooglePhotosLibraryApiClientSecret));
+
+        Assert.False(string.IsNullOrEmpty(
+            EnvironmentVariableManager.GooglePhotosLibraryApiProjectId));
+
+        Assert.False(string.IsNullOrEmpty(
+            EnvironmentVariableManager.SteamWebApiClientApiKey));
+
+        Assert.False(string.IsNullOrEmpty(
+            EnvironmentVariableManager.SteamWebApiStoreLanguage));
+    }
+}
