d3a2ea618cb837c5ab85e49188dcf3f5c7a3ed2c
Kristoffer Heia <kristoffer.heia@visma.com>
2026-03-13 10:33:02 +0100

    Marked which methods in IFileStorageService should return Result

--- JottaShift/JottaShift.Core/FileStorage/IFileStorageService.cs
+++ JottaShift/JottaShift.Core/FileStorage/IFileStorageService.cs
@@ -1,30 +1,39 @@
 ﻿namespace JottaShift.Core.FileStorage;
 
+/// <remarks>
+/// Return-types are currently being converted to Result
+/// </remarks>
 public interface IFileStorageService
 {
+    // Done
     Result IsValidFileName(string fileNameWithExtension);
 
     Result<string> GetFileName(string fileFullPath);
-    
-    Task<Result<byte[]>> GetFileBytesAsync(string fileFullPath);
 
     Result<string?> SearchFileByExactName(string folderPath, string fileName, bool searchRecursively = true);
 
-    bool DeleteFile(string fileFullPath);
-    bool DeleteDirectoryContent(string directoryFullPath);
-    Task<CopyAsyncResult> CopyAsync(string sourceFileFullPath, string targetDirectory, CancellationToken ct = default);
+    Task<Result<byte[]>> GetFileBytesAsync(string fileFullPath);
 
-    bool ValidateDirectory(DirectoryOptions options);
-
+    // Keep current
     IEnumerable<string> EnumerateDirectories(string directoryFullPath);
 
     IEnumerable<string> EnumerateFiles(string directoryFullPath);
 
+
+    // TODO
     DateTime GetImageDate(string fileFullPath);
 
-    string GetImageResolution(string fileFullPath);
+    bool DeleteFile(string fileFullPath);
+
+    bool DeleteDirectoryContent(string directoryFullPath);
 
     bool FilesAreBitPerfectMatch(string pathA, string pathB, int bufferSize = 1024 * 1024);
 
     bool DoesFileMetadataMatch(string pathA, string pathB);
+
+    bool ValidateDirectory(DirectoryOptions options);
+
+    string GetImageResolution(string fileFullPath);
+
+    Task<CopyAsyncResult> CopyAsync(string sourceFileFullPath, string targetDirectory, CancellationToken ct = default);
 }
