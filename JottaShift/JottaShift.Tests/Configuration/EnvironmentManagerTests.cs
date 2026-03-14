51f8c8bb6d4096e647d5c6e08e95cf0761f5e34b
Kristoffer Heia <kristoffer.heia@protonmail.com>
2026-03-14 08:06:19 +0100

    GetKnownMachineName

--- JottaShift/JottaShift.Tests/Configuration/EnvironmentManagerTests.cs
+++ JottaShift/JottaShift.Tests/Configuration/EnvironmentManagerTests.cs
@@ -6,6 +6,15 @@ public class EnvironmentManagerTests
 {
     [Fact]
     [Trait("Dependency", "Env")]
+    public void GetKnownMachineName_ShouldReturnKnownName()
+    {
+        var result = EnvironmentManager.GetKnownMachineName();
+
+        ResultAssert.ValueSuccess(result, "BRUTUS");
+    }
+
+    [Fact]
+    [Trait("Dependency", "Env")]
     public void ValidateAllEnvironmentVariablesForApplication()
     {
         Assert.False(string.IsNullOrEmpty(
