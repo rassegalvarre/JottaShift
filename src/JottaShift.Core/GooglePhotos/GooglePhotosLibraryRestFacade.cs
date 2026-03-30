7d552d722a58b03396a0481510669636896f568a
Kristoffer Heia <kristoffer.heia@visma.com>
2026-03-30 10:50:33 +0200

    DI

--- src/JottaShift.Core/GooglePhotos/GooglePhotosLibraryRestFacade.cs
+++ src/JottaShift.Core/GooglePhotos/GooglePhotosLibraryRestFacade.cs
@@ -1,11 +1,14 @@
 ﻿using Google.Apis.PhotosLibrary.v1.Data;
+using Microsoft.Extensions.Logging;
 
 namespace JottaShift.Core.GooglePhotos;
 
 /// <summary>
 /// Implementation of <see cref="IGooglePhotosLibraryFacade"/> using REST API.
 /// </summary>
-internal class GooglePhotosLibraryRestFacade : IGooglePhotosLibraryFacade
+internal class GooglePhotosLibraryRestFacade(
+    IUserCredentialManager _userCredentialManager,
+    ILogger<GooglePhotosLibraryRestFacade> _logger) : IGooglePhotosLibraryFacade
 {
     public Task<Result<BatchCreateMediaItemsResponse>> AddImagesToAlbum(string albumId, IEnumerable<string> uploadTokens)
     {
