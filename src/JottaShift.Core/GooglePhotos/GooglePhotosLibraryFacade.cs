569575622b86376a93097eb794b34746d1bfc24f
Kristoffer Heia <kristoffer.heia@protonmail.com>
2026-04-03 14:24:46 +0200

    Fixed object being logged instead of .Count (#59)

--- src/JottaShift.Core/GooglePhotos/GooglePhotosLibraryFacade.cs
+++ src/JottaShift.Core/GooglePhotos/GooglePhotosLibraryFacade.cs
@@ -156,7 +156,7 @@ public class GooglePhotosLibraryFacade : IGooglePhotosLibraryFacade
             }
 
             _logger.LogInformation("Uploaded {ItemCount} items to Google Photos album with Id {AlbumId}",
-                batchCreateResponse.NewMediaItemResults, albumId);
+                batchCreateResponse.NewMediaItemResults.Count, albumId);
 
             return Result<BatchCreateMediaItemsResponse>.Success(batchCreateResponse);
         }
