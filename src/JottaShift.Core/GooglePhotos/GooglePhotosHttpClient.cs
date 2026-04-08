e875d16dcb8f7c6cd4fe84085144cc37472d0ff0
Kristoffer Heia <kristoffer.heia@visma.com>
2026-04-08 14:05:09 +0200

    Refactor

--- src/JottaShift.Core/GooglePhotos/GooglePhotosHttpClient.cs
+++ src/JottaShift.Core/GooglePhotos/GooglePhotosHttpClient.cs
@@ -3,8 +3,6 @@ using JottaShift.Core.FileStorage;
 using JottaShift.Core.HttpClientWrapper;
 using Microsoft.Extensions.Logging;
 using System.Net.Http.Headers;
-using System.Reflection.Metadata;
-using static Google.Apis.PhotosLibrary.v1.MediaItemsResource;
 
 namespace JottaShift.Core.GooglePhotos;
 
@@ -15,13 +13,13 @@ public class GooglePhotosHttpClient(
     ILogger<GooglePhotosHttpClient> _logger) : IGooglePhotosHttpClient, IGooglePhotosLibraryFacade
 {
 
-    private HttpContent SerializeToJsonContent<T>(T obj)
+    private static StringContent SerializeToStringContent<T>(T obj)
     {
         var json = System.Text.Json.JsonSerializer.Serialize(obj);
         return new StringContent(json, System.Text.Encoding.UTF8, "application/json");
     }
 
-    private HttpContent SerializeToByteArrayContent(byte[] bytes)
+    private static ByteArrayContent SerializeToByteArrayContent(byte[] bytes)
     {
         return new ByteArrayContent(bytes)
         {
@@ -58,8 +56,9 @@ public class GooglePhotosHttpClient(
         return response.ToResult();
     }
 
-    // Replace with?
-    // https://developers.google.com/photos/library/reference/rest/v1/mediaItems/batchCreate
+    /// <remarks>
+    /// <see href="https://developers.google.com/photos/library/guides/upload-media">Upload media</see>
+    /// </remarks>
     public async Task<Result<string>> UploadPhotoAsync(string fileFullPah)
     {
         var fileContentResult = await _fileStorage.GetFileBytesAsync(fileFullPah);
@@ -79,25 +78,27 @@ public class GooglePhotosHttpClient(
         }
 
         const string requestUri = "https://photoslibrary.googleapis.com/v1/uploads";
-
         var content = SerializeToByteArrayContent(fileContentResult.Value);
+        var additionalHeaders = new Dictionary<string, string>()
+        {
+            { "X-Goog-Upload-File-Name", fileNameResult.Value },
+            { "X-Goog-Upload-Protocol", "raw" }
+        };
 
         var result = await SendWithBearerTokenAsync<string>(
             requestUri,
             HttpMethod.Post,
             content,            
-            new Dictionary<string, string>() // Required headers (see Google docs)
-            {
-                { "X-Goog-Upload-File-Name", fileNameResult.Value },
-                { "X-Goog-Upload-Protocol", "raw" }
-            });
+            additionalHeaders);
 
         return result;
     }
 
-    
-    
-    // https://developers.google.com/photos/library/reference/rest/v1/albums/batchAddMediaItems
+
+
+    /// <remarks>
+    /// <see href="https://developers.google.com/photos/library/reference/rest/v1/albums/batchAddMediaItems">Method: albums.batchAddMediaItems</see>
+    /// </remarks>
     public async Task<Result<BatchCreateMediaItemsResponse>> AddImagesToAlbum(string albumId, IEnumerable<string> uploadTokens)
     {
         var batchCreateRequest = new BatchCreateMediaItemsRequest
@@ -110,7 +111,7 @@ public class GooglePhotosHttpClient(
         };
 
         string requestUri = $"https://photoslibrary.googleapis.com/v1/albums/{albumId}:batchAddMediaItems";
-        var content = SerializeToJsonContent(batchCreateRequest);
+        var content = SerializeToStringContent(batchCreateRequest);
 
         var batchCreateResponse = await SendWithBearerTokenAsync<BatchCreateMediaItemsResponse>(
             requestUri,
@@ -120,7 +121,9 @@ public class GooglePhotosHttpClient(
         return batchCreateResponse;
     }
 
-    // https://developers.google.com/photos/library/reference/rest/v1/albums/create
+    /// <remarks>
+    /// <see href="https://developers.google.com/photos/library/reference/rest/v1/albums/create">Method: albums.create</see>
+    /// </remarks>
     public async Task<Result<Album>> CreateAlbumAsync(string albumName)
     {
         var albumRequest = new CreateAlbumRequest()
@@ -132,7 +135,7 @@ public class GooglePhotosHttpClient(
         };
 
         string requestUri = "https://photoslibrary.googleapis.com/v1/album";
-        var content = SerializeToJsonContent(albumRequest);
+        var content = SerializeToStringContent(albumRequest);
 
         var createAlbumResponse = await SendWithBearerTokenAsync<Album>(
             requestUri,
@@ -142,7 +145,9 @@ public class GooglePhotosHttpClient(
         return createAlbumResponse;
     }
 
-    // https://developers.google.com/photos/library/reference/rest/v1/albums/get
+    /// <remarks>
+    /// <see href="https://developers.google.com/photos/library/reference/rest/v1/albums/get">Method: albums.get</see>
+    /// </remarks>
     public async Task<Result<Album>> GetAlbumFromIdAsync(string albumId)
     {
         string requestUri = $"https://photoslibrary.googleapis.com/v1/albums/{albumId}";
@@ -152,11 +157,13 @@ public class GooglePhotosHttpClient(
         return getAlbumResponse;
     }
 
-    // https://developers.google.com/photos/library/reference/rest/v1/albums/list
+    // TODO: Extract List to own method
+    /// <remarks>
+    /// Uses <see href="https://developers.google.com/photos/library/reference/rest/v1/albums/list">Method: albums.list</see>
+    /// </remarks>
     public async Task<Result<Album>> GetAlbumFromTitleAsync(string albumName)
     {
         string requestUri = $"https://photoslibrary.googleapis.com/v1/albums/";
-
 
        var getAlbumsResponse = await SendWithBearerTokenAsync<ListAlbumsResponse>(requestUri, HttpMethod.Get);
 
